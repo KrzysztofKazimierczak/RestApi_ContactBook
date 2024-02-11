a8b1dc2c00d1faf4f1b87d3cebfe983cb2a16705
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-11 15:52:39 +0100

    update routes for logged user

--- routes/contacts.py
+++ routes/contacts.py
@@ -6,45 +6,61 @@ from sqlalchemy.orm import Session
 from database.db import get_db
 from schemas import ContactModel, ContactUpdate
 from repository import contacts as repository_contacts
+from database.models import User
+from services.auth import auth_service
 
 router = APIRouter(prefix='/contacts')
 
 
 
 @router.post("/", response_model=ContactModel)
-async def create_contact(body: ContactModel, db: Session = Depends(get_db)):
-    return await repository_contacts.create_contact(body, db)
+async def create_contact(body: ContactModel, db: Session = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    return await repository_contacts.create_contact(body, current_user, db)
+
 
 @router.get("/", response_model=list[ContactModel])
-async def get_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    contacts = await repository_contacts.get_contacts(skip, limit, db)
-    return contacts
+async def get_contacts(skip: int = 0, limit: int = 20, db: Session = Depends(get_db),
+                        current_user: User = Depends(auth_service.get_current_user)):
+    if skip < 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="A negative number is used.")
+    elif limit <= skip:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="The limit is less than or equal to the skip.")
+
+    contact = await repository_contacts.get_contacts(skip, limit, current_user, db)
+    return contact
 
 
 @router.get("/{contact_id}", response_model=ContactModel)
-async def get_contact(contact_id: int, db: Session = Depends(get_db)):
-    contact = await repository_contacts.get_contact(contact_id, db)
+async def get_contact(contact_id: int, db: Session = Depends(get_db),
+                       current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repository_contacts.get_contact(contact_id, current_user, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found.")
     return contact
+
 
 @router.put("/{contact_id}", response_model=ContactModel)
-async def update_contact(contact_id: int, body: ContactUpdate, db: Session = Depends(get_db)):
-    contact = await repository_contacts.update_contact(contact_id, body, db)
+async def update_contact(contact_id: int, body: ContactModel, db: Session = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repository_contacts.update_contact(contact_id, body, current_user, db)
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found.")
     return contact
 
+
 @router.delete("/{contact_id}", response_model=ContactModel)
-async def delete_contact(contact_id: int, db: Session = Depends(get_db)):
-    contact = await repository_contacts.delete_contact(contact_id, db)    
+async def delete_contact(contact_id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repository_contacts.delete_contact(contact_id, current_user, db)    
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
     return contact
 
 @router.get("/birthday/", response_model=List[ContactModel])
-async def get_upcoming_birthdays(db: Session = Depends(get_db)):
-    birthdays = await repository_contacts.get_upcoming_birthdays(db)
-    if birthdays is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
-    return birthdays
+async def get_upcoming_birthdays(db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repository_contacts.get_upcoming_birthdays(current_user, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found.")
+    return contact
