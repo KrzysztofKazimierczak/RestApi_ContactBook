14f41cb5ec1d1e8d9aad46c8f3a665997825a49e
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-11 15:48:09 +0100

    add User models to schemas

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
-from datetime import date
-from pydantic import BaseModel
+from datetime import date, datetime
+from pydantic import BaseModel, Field, EmailStr
 
 
 
@@ -11,6 +11,9 @@ class ContactModel(BaseModel):
     birth_date: date
     extra_data: str = None
 
+    class Config:
+        from_attributes = True
+
 class ContactUpdate(BaseModel):
     first_name: str = None
     last_name: str = None
@@ -18,3 +21,30 @@ class ContactUpdate(BaseModel):
     phone_number: str = None
     birth_date: date = None
     extra_data: str = None
+
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=10)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
