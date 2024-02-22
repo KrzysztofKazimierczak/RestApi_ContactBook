b7df8056ab2a4ce040a6551665d9e8d28204ff08
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-22 19:08:11 +0100

    add fastapi_limiter and CORS

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Depends
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+from fastapi.middleware.cors import CORSMiddleware
+import redis.asyncio as redis
 
 from routes import contacts, auth
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(contacts.router, prefix='/api')
 app.include_router(auth.router, prefix='/api')
 
-@app.get("/")
+rate_limit = RateLimiter(times=10, seconds=60)
+
+@app.on_event("startup")
+async def startup():
+    r = await redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=os.getenv("REDIS_DB"), encoding="utf-8", decode_responses=True)
+    await FastAPILimiter.init(r)
+
+@app.get("/", dependencies=[Depends(rate_limit)])
 def read_root():
     return {"message": "Hello World"}
